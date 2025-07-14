87dfe55b265c9c0f7bebe7bc7d4e9555f7c53044
Deepali Jain <deepali.jain@joshtechnologygroup.com>
2025-07-14 14:09:25 +0530

    Header section: Completed the header section

--- src/script/index.js
+++ src/script/index.js
@@ -1,1 +1,13 @@
 import '../styles/main.scss';
+
+const hamIcon = document.getElementById('js-hamIcon');
+const crossIcon = document.getElementById('js-crossIcon');
+const navLinks = document.getElementById('js-navLinks');
+
+hamIcon.addEventListener('click', () => {
+    navLinks.classList.add('open');
+});
+
+crossIcon.addEventListener('click', () => {
+    navLinks.classList.remove('open');
+});
